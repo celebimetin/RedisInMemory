58ec46a094d626c98b5e61405099fad671a9d814
Metin Çelebi <metincelebi5534@gmail.com>
2023-05-15 16:34:28 +0300

    Add project files.

---
+++ InMemory.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.VisualBasic;
+
+namespace InMemory.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.zaman = memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
